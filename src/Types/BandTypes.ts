a6e073676c3574e95605948bb0d491d258c1ab91
berbraga <bernardo.sbraga@yahoo.com>
2022-07-15 16:41:47 -0300

    bernado : terminei a parte da banda

---
+++ src/Types/BandTypes.ts
@@ -0,0 +1,6 @@
+export type BandaCreate = { // nome, gênero musical principal a qual ela se identifica e o nome de um responsável (que pode ser qualquer membro dela)
+    name: string,
+    music_genre: string,
+    responsible: string
+
+}
